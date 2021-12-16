39939a2aae184012a9d54708dfee9a994fae2728
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-12-16 09:32:36 -0500

    working on problem pattern problems

--- problem-patterns/index.js
+++ problem-patterns/index.js
@@ -1,1 +1,40 @@
-console.log("Hello World!");
+// Example of a Frequency Pattern
+// Problem: Write a function called same, which accepts two arrays.
+// The function should return true if every value in the array has it's corresponding value squared in the second array
+// The frequency of values must be the same.
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+
+// Frequency Counter Problem #1  - Given two strings, write a function to determine if the second string is an anagram of the first.
+// An anagram is a word, phrases, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function validAnagram(str1, str2) {
+  // Enter code here
+}
