50a0a3b796c3fab59261fc216295f70cc7ccdd2a
Spoon <spoon@Spoons-MacBook-Air.local>
2022-04-16 13:31:39 -0400

    added stop gap from uncessary iteration, and a 2nd solution

--- sort/index.js
+++ sort/index.js
@@ -1,6 +1,8 @@
-// Bubble Sort *unoptimized approach*
+// Solution #1: Bubble Sort *Unoptimized Approach*
 function bubbleSort(arr) {
-    // Sets the position of where to stop the inner loop
+  // Variable stops unccessary iteration
+  let noSwap;
+  // Sets the position of where to stop the inner loop
   for (let i = arr.length; i > 0; --i) {
     // Loop for the logic starting at the first position
     for (let j = 0; j < i - 1; ++j) {
@@ -11,9 +13,29 @@ function bubbleSort(arr) {
         arr[j] = arr[j + 1];
         // Sets the new largest val
         arr[j + 1] = temp;
+        noSwap = false;
       }
     }
+    if (noSwap) break;
   }
   return arr;
 }
 
+// Solution #2: Bubble Sort *Unoptimized Approach*
+function bubbleSort2(arr) {
+  const swap = (arr, idx, idx2) => {
+    [arr[idx], arr[idx2]] = [arr[idx2], arr[idx]];
+  };
+
+  for (let i = arr.length; i > 0; --i) {
+    let noSwap;
+    for (let j = 0; j < i - 1; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+}
