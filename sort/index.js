087a9ba7ecac0ae30b4354fb84bdb3401b39b785
Spoon <spoon@Spoons-MacBook-Air.local>
2022-04-16 12:33:11 -0400

    added comments to bubble sort

--- sort/index.js
+++ sort/index.js
@@ -1,12 +1,17 @@
 // Bubble Sort *unoptimized approach*
 function bubbleSort(arr) {
+    // Sets the position of where to stop the inner loop
   for (let i = arr.length; i > 0; --i) {
+    // Loop for the logic starting at the first position
     console.log("Position of i", i);
     for (let j = 0; j < i - 1; ++j) {
       console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
+        // Sets the current largest val
         let temp = arr[j];
+        // Sets the current val to the largest val
         arr[j] = arr[j + 1];
+        // Sets the new largest val
         arr[j + 1] = temp;
       }
     }
