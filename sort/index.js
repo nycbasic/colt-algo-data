d11a16a15757d73f684fec3973bbac2976cc1310
Spoon <spoon@Spoons-MacBook-Air.local>
2022-04-16 12:27:49 -0400

    unoptimized bubble sort

--- sort/index.js
+++ sort/index.js
@@ -1,4 +1,17 @@
-// Bubble Sort 
-function bubbleSort() {
-    
+// Bubble Sort *unoptimized approach*
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; --i) {
+    console.log("Position of i", i);
+    for (let j = 0; j < i - 1; ++j) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
+
+console.log(bubbleSort([37, 28, 45, 8]));
