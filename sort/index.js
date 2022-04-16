b02c812eae225273b665272028ee74523517075b
Spoon <spoon@Spoons-MacBook-Air.local>
2022-04-16 12:38:06 -0400

    removed the console logs from bubble sort

--- sort/index.js
+++ sort/index.js
@@ -3,9 +3,7 @@ function bubbleSort(arr) {
     // Sets the position of where to stop the inner loop
   for (let i = arr.length; i > 0; --i) {
     // Loop for the logic starting at the first position
-    console.log("Position of i", i);
     for (let j = 0; j < i - 1; ++j) {
-      console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         // Sets the current largest val
         let temp = arr[j];
@@ -19,4 +17,3 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort([37, 28, 45, 8]));
